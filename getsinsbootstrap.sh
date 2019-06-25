1b5f369c11b9c194ba81d23bb8d0d7c935ff4cc5
deadthings <33961641+deadthings@users.noreply.github.com>
2019-06-25 11:10:36 -0400

    Create getsinsbootstrap.sh

---
+++ getsinsbootstrap.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+fileid="1hALfM3T_qR5OIkHgslEIc7V2HHJKOLLm"
+filename="bootstrap.dat.zip"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
