49d51bca19232ea67622be56552eb5cc805f455a
deadthings <33961641+deadthings@users.noreply.github.com>
2019-06-25 11:30:20 -0400

    Update getsinsbootstrap.sh

--- getsinsbootstrap.sh
+++ getsinsbootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+apt update && apt install zip unzip
 fileid="1hALfM3T_qR5OIkHgslEIc7V2HHJKOLLm"
 filename="bootstrap.dat.zip"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+unzip ${filename}
+rm ${filename} cookie
